8bbfa870b8ba6a353705b52b6d1a0114fd558e89
NDW <cryptondee@gmail.com>
2023-09-28 12:17:03 +0200

    replaced typeform for tally

--- components/Contact.js
+++ components/Contact.js
@@ -71,7 +71,7 @@ export default function Contact() {
             </dl>
           </div>
         </div>
-        <Widget id="ibaGG4n6" style={{ width: "100%" }} className="my-form" />
+        <iframe data-tally-src="https://tally.so/embed/3Elqg4?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1" loading="lazy" width="100%" height="476" frameborder="0" marginheight="0" marginwidth="0" title="Contact form"></iframe><script>var d=document,w="https://tally.so/widgets/embed.js",v=function(){"undefined"!=typeof Tally?Tally.loadEmbeds():d.querySelectorAll("iframe[data-tally-src]:not([src])").forEach((function(e){e.src=e.dataset.tallySrc}))};if("undefined"!=typeof Tally)v();else if(d.querySelector('script[src="'+w+'"]')==null){var s=d.createElement("script");s.src=w,s.onload=v,s.onerror=v,d.body.appendChild(s);}</script>
       </div>
     </div>
   );
